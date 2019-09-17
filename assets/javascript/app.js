d8f11dcc08740bd2a85df97dd7b692da67b48ffa
Grievous Angel <ronfud2000@gmail.com>
2019-09-16 21:30:55 -0400

    added event handler to control gif animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,48 +35,65 @@ $("#add-topic-btn").on("click", function(event) {
         displayButtons();
     });
     
-    // add click event to all images for displaying topic gifs
-    $(document).on("click", ".topic-btn", processMovie);
-    // intial call to display buttons
-    displayButtons();
+// add click event to all images for displaying topic gifs
+$(document).on("click", ".topic-btn", processMovie);
+// intial call to display buttons
+displayButtons();
+function processMovie() {
+    // get topic for gifs to be retrieved
+    var got_topic = $(this).attr("topic");
+    // create giphy api search string with api key and limit of 10 gifs retrieved
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        got_topic + "&api_key=tiqUaUBX3u8WwTeMQK4mUBb2X8AdvqPa&limit=10";
+    // perform ajax query
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    // set up response when promise completed
+    .then(function(response) {
+        // store results (all 10) from website in variable
+        var results = response.data;
+        // create divs for each of 10 gifs
+        for (var k = 0; k < results.length; k++) {
+            // variable for div creation
+            var giffy = $("<div>");
+            // variable for p creationg
+            var para = $("<p>").text("Rating " + rating);
+            // variable to create img
+            var image = $("<img>");
+            // variable to hold rating for each gif
+            var rating = results[k].rating;
+            // add class gif to image div
+            image.addClass("gif");
+            // add atrribute for giphy source to image div
+            image.attr("src", results[k].images.fixed_height_still.url);
+            // prepend p and img elements
+            giffy.prepend(para);
+            giffy.prepend(image);
+            // prepend div to display gif
+            $("#display-gifs").prepend(giffy);
+        }
+    });
+}
 
-    function processMovie() {
-        // get topic for gifs to be retrieved
-        var got_topic = $(this).attr("topic");
-        // create giphy api search string with api key and limit of 10 gifs retrieved
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            got_topic + "&api_key=tiqUaUBX3u8WwTeMQK4mUBb2X8AdvqPa&limit=10";
-        // perform ajax query
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        // set up response when promise completed
-        .then(function(response) {
-            // store results (all 10) from website in variable
-            var results = response.data;
-            // create divs for each of 10 gifs
-            for (var k = 0; k < results.length; k++) {
-                // variable for div creation
-                var giffy = $("<div>");
-                // variable for p creationg
-                var para = $("<p>").text("Rating " + rating);
-                // variable to create img
-                var image = $("<img>");
-                // variable to hold rating for each gif
-                var rating = results[k].rating;
-                // add class gif to image div
-                image.addClass("gif");
-                // add atrribute for giphy source to image div
-                image.attr("src", results[k].images.fixed_height_still.url);
-                // prepend p and img elements
-                giffy.prepend(image);
-                giffy.prepend(para);
-                // prepend div to display gif
-                $("#display-gifs").prepend(giffy);
-
-            }
-        });
+// create event handler to animate and de-animate gifs
+$(document).on("click", ".gif", function() {
+    // create variable for src attribute for image click on
+    var src = $(this).attr("src");
+    //  change gif from playing to non-playing state
+    if ($(this).hasClass("playing")) {
+        // replace src with non-playing gif image
+        $(this).attr("src", src.replace(/\.gif/i, "_s.gif"))
+        // remove 'playing' class from gif
+        $(this).removeClass("playing");
+    } else {
+        //change gif from non-playing to playing state
+        // add 'playing' class to gif
+        $(this).addClass("playing");
+        // replace src with playing gif image
+        $(this).attr("src", src.replace(/\_s.gif/i, ".gif"))
     }
 
+})
 
